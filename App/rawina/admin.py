091b428bb4b500a4c978a24aa6c265f50e6f8b6f
Khadaassi <aassikhadija@yahoo.fr>
2025-07-20 07:48:15 +0200

    starting fresh

--- App/rawina/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-from rawina.models import Story
-
-admin.site.register(Story)
-
-admin.site.site_header = _("Rawina Admin")
-admin.site.site_title = _("Rawina Admin Portal")
-admin.site.index_title = _("Welcome to the Rawina Admin Portal")
